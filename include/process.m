2fa8ec38d4c72fbc837f462b18f8c7be475c60f9
jtib <juliette.tib@gmail.com>
2017-03-02 14:12:53 -0500

    corrected error in analysis process

--- include/process.m
+++ include/process.m
@@ -1,19 +1,19 @@
 input('To start, enter the name of your project directory: ', 's');
 
 fprintf('Analyzing project contents...\n')
-pause(6)
+%pause(6)
 
 fprintf('Analysis finished. Strike any key to continue.\n');
 pause
 
 fprintf('It appears you will need some additional tools.\n')
 fprintf('Preparing tools archive...\n');
-pause(4)
+%pause(4)
 
 fprintf('Download link: https://frama.link/matlab-pen-paper\n');
 
 pb = input('Problem with the link above? [Y/n] ', 's');
-if length(pb) == 0 || pb == 'y' || pb == 'yes' || pb == 'Y'
+if ((length(pb) == 0) | (pb == 'y') | (pb == 'yes') | (pb == 'Y'))
     run('../utils/atama')
 end
 
